c8c717a35e0e58b65db32d6ea0f9f6bb91b05672
angelogarcia <angelogarcia@deltek.com>
2018-03-14 18:40:02 +0800

    update

--- resources/views/email/testmail.blade.php
+++ resources/views/email/testmail.blade.php
@@ -5,7 +5,7 @@
 <body>
 	<p>Hi All,</p>
 	<p>We receive a new application from {{$name}}, who wants to borrow <b>{{ $amount }} PHP</b> for {{ $terms }} days.</p>
-	<p>Kindly login to our dashboard to review his/her documents.</p>
+	<p>Kindly <a href="www.fastcashpinoy.com/index.php/login">login</a> to our dashboard to review his/her documents.</p>
 	
 	<p>Regards,</p>
 	<p>Team FCP</p>
