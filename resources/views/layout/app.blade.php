5dc4658e030c359311ab523c348bb06ef0abb9c5
angelogarcia <angelogarcia@deltek.com>
2018-03-14 16:53:44 +0800

    update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,7 @@
   <title>FastCashPinoy Philippines | Get a loan from FastCashPinoy</title>
   <meta content="0;URL='http://fastcashpinoy.com/'" />  
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="description" content="" />
   <meta name="author" content="" />
   <meta http-equiv="cache-control" content="no-cache" />
